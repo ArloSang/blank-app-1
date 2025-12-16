0ef9f4922e70bc1efa4a9ae786fb43a9f13d8d4f
ArloSang <163200786+ArloSang@users.noreply.github.com>
2025-12-16 20:36:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
-
+import os
 from supabase import create_client, Client
 url: str = os.environ.get("db.babnnvuqximsunvrxyal.supabase.co")
 key: str = os.environ.get("abbgC6AYLZevUZZl")
